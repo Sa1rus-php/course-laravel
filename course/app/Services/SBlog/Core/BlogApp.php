be75afdcccefbfe3108e17adcfab278158909b64
rekhlitskiy <drekhlitskii@gmail.com>
2022-03-20 23:22:30 +0200

    add: singletone pattern and autoload php files

---
+++ course/app/Services/SBlog/Core/BlogApp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services\SBlog\Core;
+
+use App\Services\SBlog\Core\Registry;
+class BlogApp
+{
+
+    public static $app;
+
+    public static function get_instance()
+    {
+    self::$app = Registry::instance();
+    self::getParams();
+        return self::$app;
+    }
+
+    protected static function getParams(){
+        $params = require CONF . '/params.php';
+
+        if (!empty($params)){
+            foreach ($params as $k => $v){
+                self::$app->setProperty($k,$v);
+            }
+        }
+    }
+}
